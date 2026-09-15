3dfc4c37db165663e1eff9550a8f9fc4b6630d42
murilo0410 <murilodomingos441@gmail.com>
2026-09-15 08:59:06 -0300

    Add files via upload

---
+++ memory_card_232/README.md
@@ -0,0 +1,3 @@
+# memory_card_232
+Repositório do projeto do jogo da memória
+<img width="835" height="657" alt="image" src="https://github.com/user-attachments/assets/f6b70cec-a104-4447-a7e1-6029bb4034b8" />
---
+++ memory_card_232/js/scripts.js
@@ -0,0 +1,6 @@
+// ======================================================
+// JOGO DA MEMÓRIA - Versão simplificada
+// ======================================================
+
+// Pega todas as cartas do jogo (elementos com a classe "memory-card" no HTML)
+const cards = document.querySelectorAll('.memory-card');
